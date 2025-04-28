13c272da3c3cc6eec8143912b655a36e930f21b5
dneplokhov <neplohoff@gmail.com>
2025-04-28 11:15:25 +0300

    cleanup

--- src/main/java/co/spribe/pc/TestDataHelper.java
+++ src/main/java/co/spribe/pc/TestDataHelper.java
@@ -1,7 +1,5 @@
 package co.spribe.pc;
 
-import co.spribe.pc.dto.PlayerDto;
-
 import java.util.UUID;
 
 public class TestDataHelper {
@@ -16,38 +14,5 @@ public class TestDataHelper {
 
     public static String validPassword() {
         return UUID.randomUUID().toString().substring(0, 8);
-    }
-
-    public static PlayerDto randomPlayer() {
-        return new PlayerDto(
-                25,
-                "male",
-                randomLogin(),
-                validPassword(),
-                "user",
-                randomScreenName()
-        );
-    }
-
-    public static PlayerDto randomAdmin() {
-        return new PlayerDto(
-                25,
-                "male",
-                randomLogin(),
-                validPassword(),
-                "admin",
-                randomScreenName()
-        );
-    }
-
-    public static PlayerDto randomSupervisor() {
-        return new PlayerDto(
-                25,
-                "male",
-                randomLogin(),
-                validPassword(),
-                "supervisor",
-                randomScreenName()
-        );
     }
 }
